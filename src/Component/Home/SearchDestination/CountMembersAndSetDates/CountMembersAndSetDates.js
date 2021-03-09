59cb25d748f47148a13e1e32c07ea44585156152
as ahmed <rakibasrr13@gmail.com>
2021-03-09 20:26:46 +0300

    home page 80% completed

---
+++ src/Component/Home/SearchDestination/CountMembersAndSetDates/CountMembersAndSetDates.js
@@ -0,0 +1,180 @@
+import React, { useReducer, useState } from 'react';
+import 'date-fns';
+import Grid from '@material-ui/core/Grid';
+import DateFnsUtils from '@date-io/date-fns';
+import {
+    MuiPickersUtilsProvider,
+    KeyboardDatePicker,
+} from '@material-ui/pickers';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMinus, faPlus } from '@fortawesome/free-solid-svg-icons';
+import { Col, Row } from 'react-bootstrap';
+import Shake from 'react-reveal/Shake';
+
+const initialState = {
+    adults: 0,
+    child: 0,
+    babies: 0,
+};
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'INCREMENT1':
+            return { ...state, adults: state.adults + 1 };
+        case 'DECREMENT1':
+            const adults = state.adults < 1;
+            return { ...state, adults: adults ? 0 : state.adults - 1 };
+        case 'INCREMENT2':
+            return { ...state, child: state.child + 1 };
+        case 'DECREMENT2':
+            const Child = state.child < 1;
+            return { ...state, child: Child ? 0 : state.child - 1 };
+        case 'INCREMENT3':
+            return { ...state, babies: state.babies + 1 };
+        case 'DECREMENT3':
+            const babies = state.babies < 1;
+            return { ...state, babies: babies ? 0 : state.babies - 1 };
+        default: throw new Error('Unexpected action');
+    };
+};
+
+
+const CountMembersAndSetDates = () => {
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    // date picker here ==============>
+    const [selectedStartDate, setSelectedStartDate] = useState(new Date());
+    const currentDate = new Date();
+    const futureDate = currentDate.setDate(currentDate.getDate() + 3)
+    const [selectedEndDate, setSelectedEndDate] = useState(currentDate);
+
+    const handleStartDate = startDate => {
+        setSelectedStartDate(startDate);
+    };
+    const handleEndDate = endDate => {
+        setSelectedEndDate(endDate)
+    };
+
+    // handle Members And Dates
+    const handleMembersAndDates = () => {
+        const members = state.adults > 0 || state.child > 0;
+        const startDateCheckout = selectedStartDate == "Invalid Date" || !selectedStartDate;
+        const endDateCheckout = selectedEndDate == "Invalid Date" || !selectedEndDate;
+        if (members && !startDateCheckout && !endDateCheckout) {
+            console.log(" true")
+            const remaining = { ...state, startDate: selectedStartDate, endDate: selectedEndDate };
+            fetch('http://localhost:4000/membersAndDates', {
+                method: "POST",
+                headers: { 'content-type': 'application/json' },
+                body: JSON.stringify(remaining)
+            })
+                .then(result => {
+                    if (result) {
+                        alert("data post successfully...")
+                    }
+                    console.log(result)
+                });
+        };
+
+    };
+
+
+    return (
+        <div>
+            <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                <Grid container justify="space-around">
+                    <Row className="mt-4">
+                        <Col className="p-2 shadow search_item">
+                            <div>
+                                <KeyboardDatePicker
+                                    disableToolbar
+                                    variant="inline"
+                                    format="dd/MM/yy"
+                                    margin="normal"
+                                    label="Arrival"
+                                    value={selectedStartDate}
+                                    onChange={handleStartDate}
+                                    KeyboardButtonProps={{
+                                        'aria-label': 'change date',
+                                    }}
+                                />
+                                {
+                                    selectedStartDate == null &&
+                                    <Shake>
+                                        <p className="text-danger">Start date is required</p>
+                                    </Shake>
+                                }
+                            </div>
+                        </Col>
+
+                        <Col className="p-2 ml-2 shadow search_item">
+                            <div>
+                                <KeyboardDatePicker
+                                    disableToolbar
+                                    variant="inline"
+                                    format="dd/MM/yy"
+                                    margin="normal"
+                                    label="Departure"
+                                    value={selectedEndDate}
+                                    onChange={handleEndDate}
+                                    KeyboardButtonProps={{
+                                        'aria-label': 'change date',
+                                    }}
+                                />
+                                {
+                                    selectedEndDate == null &&
+                                    <Shake>
+                                        <p className="text-danger">End date is required</p>
+                                    </Shake>
+                                }
+                            </div>
+                        </Col>
+                    </Row>
+                </Grid>
+            </MuiPickersUtilsProvider>
+            <br />
+            <div className="container-fluid px-3 shadow count_content">
+
+                <div className="py-3 border-bottom">
+                    <h6 className="text-muted mb-0">Guests</h6>
+                    <label className="mr-2 members_count" htmlFor="">{state.adults} adults,</label>
+                    <label className="mr-2 members_count" htmlFor="">{state.child} child,</label>
+                    <label className="mr-2 members_count" htmlFor="">{state.babies} babies</label>
+                </div>
+                <div className="d-flex pt-5">
+                    <h5 className="">ADULTS</h5>
+                    <div className="btn_items">
+                        <button onClick={() => dispatch({ type: 'DECREMENT1' })} className="border-0 bg-white"> <FontAwesomeIcon icon={faMinus} /></button>
+                        <label className="px-4 label_item" htmlFor="">{state.adults}</label>
+                        <button onClick={() => dispatch({ type: 'INCREMENT1' })} className="border-0 bg-white"> <FontAwesomeIcon icon={faPlus} /></button>
+                    </div>
+                </div>
+                <div className="d-flex pt-5">
+                    <div>
+                        <h5>CHILD</h5>
+                        <h6 className="text-muted">Age 2-12</h6>
+                    </div>
+                    <div className="btn_items ml-3">
+                        <button onClick={() => dispatch({ type: 'DECREMENT2' })} className="border-0 bg-white"> <FontAwesomeIcon icon={faMinus} /></button>
+                        <label className="px-4 label_item" htmlFor="">{state.child}</label>
+                        <button onClick={() => dispatch({ type: 'INCREMENT2' })} className="border-0 bg-white"> <FontAwesomeIcon icon={faPlus} /></button>
+                    </div>
+                </div>
+                <div className="d-flex pt-5">
+                    <div>
+                        <h5>BABIES</h5>
+                        <h6 className="text-muted">Younger tha 2</h6>
+                    </div>
+                    <div className="btn_item ml-2">
+                        <button onClick={() => dispatch({ type: 'DECREMENT3' })} className="border-0 bg-white"> <FontAwesomeIcon icon={faMinus} /></button>
+                        <label className="px-4 label_item" htmlFor="">{state.babies}</label>
+                        <button onClick={() => dispatch({ type: 'INCREMENT3' })} className="border-0 bg-white"> <FontAwesomeIcon icon={faPlus} /></button>
+                    </div>
+                </div>
+                <button onClick={() => handleMembersAndDates()} className="p-2 px-5 mt-5 mr-2 apply_btn">APPLY</button>
+            </div>
+        </div>
+    );
+};
+
+export default CountMembersAndSetDates;
--- src/Component/Home/SearchDestination/SearchDestination.js
+++ src/Component/Home/SearchDestination/SearchDestination.js
@@ -1,16 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import './SearchDestination.css'
-import { Col, Form, FormControl, Row, } from 'react-bootstrap';
+import { FormControl } from 'react-bootstrap';
 import { useForm } from 'react-hook-form';
-import 'date-fns';
-import Grid from '@material-ui/core/Grid';
-import DateFnsUtils from '@date-io/date-fns';
-import {
-    MuiPickersUtilsProvider,
-    KeyboardDatePicker,
-} from '@material-ui/pickers';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMinus, faPlus, faSearch } from '@fortawesome/free-solid-svg-icons';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import CountMembersAndSetDates from './CountMembersAndSetDates/CountMembersAndSetDates';
 
 // const homeData = [
 //     {
@@ -881,6 +875,7 @@ import { faMinus, faPlus, faSearch } from '@fortawesome/free-solid-svg-icons';
 
 
 const SearchDestination = ({ handleSearchResult }) => {
+    const [search, setSearch] = useState({ searchBox: '' });
     // useEffect(() => {
     //     fetch('http://localhost:4000/homePagesAllData',{
     //         method: "POST",
@@ -889,34 +884,27 @@ const SearchDestination = ({ handleSearchResult }) => {
     //     })
     // },[])
 
-    // date picker here ==============>
-    const [selectedStartDate, setSelectedStartDate] = useState(new Date());
-    // console.log(selectedStartDate, '[selectedStartDate]');
-    const [selectedEndDate, setSelectedEndDate] = useState(new Date());
-    // console.log(selectedEndDate, '[selectedEndDate]')
-    const handleStartDate = startDate => {
-        setSelectedStartDate(startDate);
-    };
-
-    const handleEndDate = endDate => {
-        setSelectedEndDate(endDate)
-    }
-
-    // form func here ===================>
-    const { register, errors, handleSubmit } = useForm();
-    const onSubmit = data => {
-        const name = data.searchBox;
-        fetch(`http://localhost:4000/destination${name}`)
+    useEffect(() => {
+        const locationName = search.searchBox;
+        const condition = locationName ? locationName : "bangladesh"
+        fetch(`http://localhost:4000/destination${condition}`)
             .then(res => res.json())
             .then(result => {
                 if (result.length > 0) {
                     handleSearchResult(result)
                 };
                 if (result.length == 0) {
-                    console.log('data empty')
+                    alert("location not found")
                 };
-            })
-    }
+            });
+    }, [search])
+
+    // form func here ===================>
+
+    const { register, errors, handleSubmit } = useForm();
+    const onSubmit = data => {
+        setSearch(data);
+    };
     return (
         <>
             <h3 className="mb-5 pb-2 item">Where do you want to go</h3>
@@ -942,106 +930,6 @@ const SearchDestination = ({ handleSearchResult }) => {
                     </datalist>
                     {errors.searchBox && <span classNane="text-danger">feild is required</span>}
                 </div>
-                <MuiPickersUtilsProvider utils={DateFnsUtils}>
-                    <Grid container justify="space-around">
-                        <Row className="mt-4">
-                            <Col className="p-2 shadow search_item">
-                                <KeyboardDatePicker
-                                    disableToolbar
-                                    variant="inline"
-                                    format="dd/MM/yy"
-                                    margin="normal"
-                                    label="Arrival"
-                                    value={selectedStartDate}
-                                    onChange={handleStartDate}
-                                    KeyboardButtonProps={{
-                                        'aria-label': 'change date',
-                                    }}
-                                />
-                            </Col>
-                            <Col className="p-2 ml-2 shadow search_item">
-                                <KeyboardDatePicker
-                                    disableToolbar
-                                    variant="inline"
-                                    format="dd/MM/yy"
-                                    margin="normal"
-                                    label="Departure"
-                                    value={selectedEndDate}
-                                    onChange={handleEndDate}
-                                    KeyboardButtonProps={{
-                                        'aria-label': 'change date',
-                                    }}
-                                />
-                            </Col>
-                        </Row>
-                    </Grid>
-                </MuiPickersUtilsProvider>
-                <br />
-                {/* <div className="multi_select_box"> */}
-                {/* <select className="multi_select px-3" multiple>
-                       <option>rakib</option>
-                       <option>raju</option>
-                       <option>rakhi</option>
-                       <option>polash</option>
-                       <option>faruk</option>
-                       <option>rihan</option>
-                    </select> */}
-                {/* </div> */}
-                {/* <Form.Group controlId="exampleForm.SelectCustom">
-                    <Form.Label>Custom select</Form.Label> */}
-                {/* <Form.Control as="select" custom> */}
-                {/* <select class="selectpicker" multiple data-max-options="2">
-                        <optgroup label="Condiments" data-max-options="2">
-                            <option>Mustard</option>
-                            <option>Ketchup</option>
-                            <option>Relish</option>
-                        </optgroup>
-                        <optgroup label="Breads" data-max-options="2">
-                            <option>Plain</option>
-                            <option>Steamed</option>
-                            <option>Toasted</option>
-                        </optgroup>
-                        <optgroup label="Breads" data-max-options="2">
-                            <option>Plain</option>
-                            <option>Steamed</option>
-                            <option>Toasted</option>
-                        </optgroup>
-                        </select> */}
-                {/* </Form.Control> */}
-                {/* </Form.Group> */}
-                <div className="container-fluid p-3 shadow count_content">
-                    <div className="d-flex pt-5">
-                        <h5 className="">ADULTS</h5>
-                        <div className="btn_items">
-                            {/* <button className="border-0 bg-white"> <FontAwesomeIcon icon={faMinus} /></button> */}
-                            <label className="px-4 label_item" htmlFor="">1</label>
-                            {/* <button className="border-0 bg-white"> <FontAwesomeIcon icon={faPlus} /></button> */}
-                        </div>
-                    </div>
-                    <div className="d-flex pt-5">
-                        <div>
-                            <h5>CHILD</h5>
-                            <h6 className="text-muted">Age 2-12</h6>
-                        </div>
-                        <div className="btn_items ml-3">
-                            {/* <button className="border-0 bg-white"> <FontAwesomeIcon icon={faMinus} /></button> */}
-                            <label className="px-4 label_item" htmlFor="">2</label>
-                            {/* <button className="border-0 bg-white"> <FontAwesomeIcon icon={faPlus} /></button> */}
-                        </div>
-                    </div>
-                    <div className="d-flex pt-5">
-                        <div>
-                            <h5>BABIES</h5>
-                            <h6 className="text-muted">Younger tha 2</h6>
-                        </div>
-                        <div className="btn_item ml-2">
-                            {/* <button className="border-0 bg-white"> <FontAwesomeIcon icon={faMinus} /></button> */}
-                            <label className="px-4 label_item" htmlFor="">3</label>
-                            {/* <button className="border-0 bg-white"> <FontAwesomeIcon icon={faPlus} /></button> */}
-                        </div>
-                    </div>
-                </div>
-                {/* <button className="p-2 px-5 my-3 mr-2 apply_btn">APPLY</button> */}
                 <button
                     type="submit"
                     className="border-0 px-5 py-3 text-white"
@@ -1049,6 +937,7 @@ const SearchDestination = ({ handleSearchResult }) => {
                     <FontAwesomeIcon className="mr-2" icon={faSearch} /> Search
                  </button>
             </form>
+            <CountMembersAndSetDates />
         </>
     );
 };
